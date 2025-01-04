1ff30ea5f1de59955c3e6f27f6aa76e753a57e5e
Hamid <aleehamid1024@gmail.com>
2025-01-04 11:38:28 +0500

    initial commit

---
+++ src/test/java/net/worth/banking_app/BankAppApplicationTests.java
@@ -0,0 +1,13 @@
+package net.worth.banking_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
